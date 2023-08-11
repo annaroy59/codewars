c5563ec8dc94bca4ccd371934861b5167236440a
annaroy59 <141730866+annaroy59@users.noreply.github.com>
2023-08-11 13:04:42 +0500

    Update Find all active students.sql

--- 8kyu/Find all active students.sql
+++ 8kyu/Find all active students.sql
@@ -1,9 +1,14 @@
--- Link
+--👁‍🗨Link
 -- https://www.codewars.com/kata/5809b9ef88b750ab180001ec
 
--- Description
+--❓Description
 -- Create a simple SELECT query to display student information of all ACTIVE students
+--TABLE STRUCTURE:
+--students
+--Id (integer)	FirstName (text)	LastName (text)	IsActive (boolean)
+--Note:
+--IsActive (true = 1 or false = 0)
 
--- Solution
+--✅Solution
 SELECT * FROM students
 WHERE IsActive
