cdc204f3a7ef5ea9e8c022495176725454abb5ec
annaroy59 <141730866+annaroy59@users.noreply.github.com>
2023-08-11 12:54:29 +0500

    Create Find all active students.sql

---
+++ 8kyu/Find all active students.sql
@@ -0,0 +1,9 @@
+-- Link
+-- https://www.codewars.com/kata/5809b9ef88b750ab180001ec
+
+-- Description
+-- Create a simple SELECT query to display student information of all ACTIVE students
+
+-- Solution
+SELECT * FROM students
+WHERE IsActive
